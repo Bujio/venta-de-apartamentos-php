22ddd14bdeb177b777aeb7348108a19f94663a7c
Javier.Vivas <79529756+Bujio@users.noreply.github.com>
2024-02-19 18:27:00 +0100

    base php and create route create property

---
+++ admin/propiedades/crear.php
@@ -0,0 +1,17 @@
+<?php
+
+require "../../includes/functions.php";
+
+incluirTemplate("header");
+
+?>
+
+<main class="contenedor seccion">
+  <h1>Crear</h1>
+
+  <a href="/admin" class="boton-verde">Volver</a>
+</main>
+
+<?php
+incluirTemplate("footer");
+?>
---
+++ base.php
@@ -0,0 +1,16 @@
+<?php
+
+require "../../includes/functions.php";
+
+incluirTemplate("header");
+
+?>
+
+<main class="contenedor seccion">
+    <h1>Titulo Página</h1>
+</main>
+
+<?php
+incluirTemplate("footer");
+?>
+
