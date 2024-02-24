9453350cb964429caed4173ee441befaf7fcb87c
Javier.Vivas <79529756+Bujio@users.noreply.github.com>
2024-02-24 13:04:48 +0100

    read of db in admin

--- admin/index.php
+++ admin/index.php
@@ -1,18 +1,66 @@
 <?php
+//Importamos conexion de DB
+require "../includes/config/database.php";
+$db = conectarDB();
+//Escribimos la query
+$queryPropiedades = "SELECT * FROM propiedades;";
 
+//Consultamos la db
+$consultaPropiedades = mysqli_query($db, $queryPropiedades);
+
+echo "<pre>";
+var_dump($consulta);
+echo "</pre>";
+
+
+//Muestra mensaje opcional
+$resultado = $_GET["resultado"] ?? null;
+
+//Incluye un template
 require "../includes/functions.php";
-
 incluirTemplate("header");
 
 ?>
 
 <main class="contenedor seccion">
   <h1>Administrador</h1>
-
+  <?php if (intval($resultado) === 1) : ?>
+    <p class="alerta correcta">Propiedad creada correctamente</p>
+  <?php endif ?>
   <a href="/admin/propiedades/crear.php" class="boton-verde">Nueva Propiedad</a>
+
+  <table class="propiedades">
+    <thead>
+      <tr>
+        <th>Id</th>
+        <th><?php echo $propiedad ?></th>
+        <th>Imagen</th>
+        <th>Precio</th>
+        <th>Acciones</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php while ($propiedad = mysqli_fetch_assoc($consultaPropiedades)) : ?>
+        <tr>
+
+          <td><?php echo $propiedad["id"] ?></td>
+          <td><?php echo $propiedad["titulo"] ?></td>
+          <td><img src="/imagenes/<?php echo $propiedad["imagen"] ?>" class="imagen-tabla"></td>
+          <td><?php echo $propiedad["precio"] . "€" ?></td>
+          <td>
+            <a class=" boton-rojo-block" href="">Eliminar</a>
+            <a class="boton-amarillo-block" href="">Actualizar</a>
+          </td>
+        </tr>
+      <?php endwhile; ?>
+    </tbody>
+  </table>
 
 </main>
 
 <?php
+
 incluirTemplate("footer");
+//Opcional: cerrar la conexion
+mysqli_close($db);
 ?>
--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -100,7 +100,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $resultado = mysqli_query($db, $query);
     if ($resultado) {
       //Redireccionar al usuario
-      header("Location: /admin");
+      header("Location: /admin?resultado=1");
     }
   }
 }
